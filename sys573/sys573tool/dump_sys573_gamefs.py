26b4a5f26ee3e402631499945825e7adea073645
987123879113 <63495610+987123879113@users.noreply.github.com>
2021-04-26 11:14:53 +0900

    Club ver 2 support

--- sys573/sys573tool/dump_sys573_gamefs.py
+++ sys573/sys573tool/dump_sys573_gamefs.py
@@ -641,6 +641,7 @@ def get_card_filenames(input_folder, main_card_filename=None):
             "GE885JAA.DAT",
             "GC845EBA.DAT",
             "GQ894JAA.DAT",
+            "GE984JAA.DAT",
             "GQ886AA.DAT",
             "GQ886EA.DAT",
             "GQ886JA.DAT",
@@ -988,6 +989,7 @@ def main():
     filetables.append((read_file_table_ddr, 0x100000, os.path.join(args.input, "GE885JAA.DAT"), 2))
     filetables.append((read_file_table_ddr, 0x100000, os.path.join(args.input, "GC845EBA.DAT"), 2))
     filetables.append((read_file_table_ddr, 0x100000, os.path.join(args.input, "GQ894JAA.DAT"), 2))
+    filetables.append((read_file_table_ddr, 0x100000, os.path.join(args.input, "GE984JAA.DAT"), 2))
     filetables.append((read_file_table_ddr_dancingstage, 0x401000, os.path.join(args.input, "GAME.DAT"), 2))
     filetables.append((read_file_table_ddr_dancingstage, 0x401000, os.path.join(args.input, "GC910JC.BIN"), 2)) # Dancing Stage
     filetables.append((read_file_table_ddr_dancingstage, 0x401000, os.path.join(args.input, "GC910JA.BIN"), 2)) # Dancing Stage
