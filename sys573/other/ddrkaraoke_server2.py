be83866ad010170f9b2646130695689f20b2ac5c
987123879113 <63495610+987123879113@users.noreply.github.com>
2022-08-15 08:57:54 +0900

    Add command to request a specific song number

--- sys573/other/ddrkaraoke_server2.py
+++ sys573/other/ddrkaraoke_server2.py
@@ -11,13 +11,22 @@ s.listen(1)
 conn, addr = s.accept()
 
 while True:
-    ch = input("Enter command (s, s1, s2): ").strip().lower()
+    ch = input("Enter command (s, sn (num), s2): ").strip().lower()
 
     if ch == "s":
-        conn.sendall(b"~\x42\x00\x00\x15\x00\x08\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + b"\x02\x02\x00\x01\x01")
+        request_no_upper = "1234"
+        request_no_lower = "1234"
+        conn.sendall(b"~\x42\x00\x00\x15\x00\x07\x02\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + b"\x02\x02\x00\x01\x01")
 
-    elif ch == "s1":
-        conn.sendall(b"~\x42\x00\x00\x15\x00\x08\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+    elif ch.startswith("sn "):
+        # Request a specific song
+        request_no_str = ch[2:].strip()
+        request_no = [int(x) for x in request_no_str.split('-')]
+        request_no_bytes = int.to_bytes(request_no[0], 2, 'big') + int.to_bytes(request_no[1], 1, 'big')
+        conn.sendall(b"~\x42\x00\x00\x15\x00" + request_no_bytes + b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00" + b"\x02\x02\x00\x01\x01")
+
+    # elif ch == "s1":
+    #     conn.sendall(b"~\x42\x00\x00\x15\x00\x07\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
 
     elif ch == "s2":
         conn.sendall(b"~\x02\x02\x00\x01\x01")
